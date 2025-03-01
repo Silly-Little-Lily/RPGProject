342aec4261ffe9a7533264ff770d5cb2514a78ae
Silly-Little-Lily <sillyorsomething@gmail.com>
2025-02-28 22:34:31 -0600

    Create species.py

---
+++ RPGProject/code/species.py
@@ -0,0 +1,32 @@
+## Species Class
+
+class Species:
+
+    def __init__(self, Id, Name, NamePlural, Category, Size, Senses, Challenge, Moves, Vulnerabilities,
+                 Resistances, Immunities, AC, HP, Strength, Dexterity, Constitution, Intelligence,
+                 Perception, Charisma, Speed, SwimSpeed, FlySpeed, SpecialAbilities, Behavior, Languages):
+        self.Id = Id
+        self.Name = Name
+        self.NamePlural = NamePlural
+        self.Category = Category
+        self.Size = Size
+        self.Senses = Senses
+        self.Challenge = Challenge
+        self.Moves = Moves
+        self.Vulnerabilities = Vulnerabilities
+        self.Resistances = Resistances
+        self.Immunities = Immunities
+        self.AC = AC
+        self.HP = HP
+        self.Strength = Strength
+        self.Dexterity = Dexterity
+        self.Constitution = Constitution
+        self.Intelligence = Intelligence
+        self.Perception = Perception
+        self.Charisma = Charisma
+        self.Speed = Speed
+        self.SwimSpeed = SwimSpeed
+        self.FlySpeed = FlySpeed
+        self.SpecialAbilities = SpecialAbilities
+        self.Behavior = Behavior
+        self.Languages = Languages
