ab717fe8e722a6d25516e7924d5bc2aeb23764da
Silly-Little-Lily <sillyorsomething@gmail.com>
2025-03-12 13:28:14 -0500

    Update game.py

--- RPGProject/code/game.py
+++ RPGProject/code/game.py
@@ -5,9 +5,13 @@ pygame.init()
 
 class Game:
 
-  def __init__(self,slot,screen,tiles,tile_categories,sprites):
+  def __init__(self,slot,screen,tiles,tile_categories,sprites,mode):
     self.slot = slot
     self.screen = screen
     self.tiles = tiles
     self.tile_categories tile_categories
     self.sprites = sprites
+
+  def run(self):
+    if mode == "overworld":
+      pass
