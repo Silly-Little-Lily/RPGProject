4e01dbf449325ae2853c9d0aab1d46d7773f38fe
Silly-Little-Lily <sillyorsomething@gmail.com>
2025-03-13 14:19:12 -0500

    Update main.py

--- RPGProject/code/main.py
+++ RPGProject/code/main.py
@@ -3,6 +3,7 @@ import start
 
 pygame.init()
 
+# Retrieving and reading global save data
 save_data_file = open("data/save/global_data.txt")
 save_data_lines = save_data_file.read()
 save_data_list = save_data_lines.strip().split(",")
@@ -10,6 +11,7 @@ save_data_list = save_data_lines.strip().split(",")
 width = int(save_data_list[0].strip())
 height = int(save_data_list[1].strip())
 
+# Setting up the screen and system
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("RPG Game")
 
